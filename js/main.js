8d9621f25c36a3646b259bd8d5fd673fc7ad4369
ekampreetkaur <113371633+ekameeraa@users.noreply.github.com>
2023-11-11 08:58:00 -0500

    java
    updating js files

--- js/main.js
+++ js/main.js
@@ -54,3 +54,59 @@
     The MouseEvent.clientX read-only property provides the horizontal coordinate within the application's client area at which the event occurred (as opposed to the coordinates within the page). For example, clicking in the top-left corner of the client area will always result in a mouse event with a clientX value of 0, regardless of whether the page is scrolled horizontally.
     */
 })();
+
+
+
+
+(() => {
+  const canvas = document.querySelector("#explode-view");
+  const context = canvas.getContext("2d");
+  canvas.width = 1920;
+  canvas.height = 1080;
+  const frameCount = 450; //how many still frames do we have?
+  const images = []; //an array to hold all of our images
+  //create an object literal with a property frame to hold the current frame
+  const buds = {
+      frame: 0
+  };
+
+  for (let i=0; i<frameCount; i++) {
+      //console.log(i);
+      //const img = new Image();
+      const img = document.createElement("img");
+      //need to recreate a string: images/explode_0001.webp
+      img.src = `images/explode_${(i+1).toString().padStart(4, '0')}.jpg`;
+      images.push(img);
+  }
+  //console.table(images)
+
+  //Not actually aniamting a DOM element, but rather an object
+  //which contains a frame count
+  gsap.to(buds, {
+      frame: 449,
+      snap: "frame",
+      scrollTrigger: {
+          trigger: "#explode-view",
+          pin: true,
+          scrub: 1,
+          start: "top top",
+          markers: true
+      },
+      onUpdate: render
+  })
+
+  images[0].addEventListener("onload", render);
+
+  function render() {
+      // console.log(buds.frame);
+      //console.log(images[buds.frame]);
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      context.drawImage(images[buds.frame], 0, 0);
+  }
+
+})();
+
+
+
+
+
